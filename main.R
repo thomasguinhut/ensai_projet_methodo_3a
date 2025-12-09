444023e548985a3105321e73764178a4a1bb2d0b
thomasguinhut <thomas.guinhut.24@gmail.com>
2025-12-09 10:30:12 +0000

    maj packages

--- main.R
+++ main.R
@@ -3,15 +3,17 @@
 ################################################################################
 
 
+
 # Importation de la fonction
 source("R/chargement_packages.R")
 
 # Définition des packages
 packages_requis <- c("dplyr", "aws.s3", "readxl", "arrow", "readr", "ggplot2",
-                     "data.table", "sf", "forcats", "leaflet", "leafgl", 
+                     "data.table", "sf", "forcats", "leaflet", 
                      "stringr", "shiny")
 
 chargement_packages(packages_requis)
+
 
 
 

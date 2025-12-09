6cc4b1711ae6a25ae8ce9587ad567bbac28ded5d
thomasguinhut <thomas.guinhut.24@gmail.com>
2025-12-09 10:44:28 +0000

    maj packages

--- main.R
+++ main.R
@@ -4,15 +4,16 @@
 
 
 
-# Importation de la fonction
-source("R/chargement_packages.R")
-
 # Définition des packages
 packages_requis <- c("dplyr", "aws.s3", "readxl", "arrow", "readr", "ggplot2",
                      "data.table", "sf", "forcats", "leaflet", 
                      "stringr", "shiny")
 
-chargement_packages(packages_requis)
+# Installation de pacman si nécessaire
+if (!require("pacman")) install.packages("pacman")
+
+# Chargement via le vecteur
+pacman::p_load(char = packages_requis)
 
 
 
